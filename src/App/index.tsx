65416a01d67235df897afa511ac4110b7e4feff1
rene-grigori <rene.tstsocal@gmail.com>
2022-06-29 13:27:20 -0700

    fix: minor editing and restructuring

---
+++ src/App/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World!</h1>
+      </header>
+    </div>
+  );
+};
